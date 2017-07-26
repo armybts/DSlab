67cd177805c40c201a116fe1d0057161d2b533ee
ghalibansari1994 <ghalibansari1994@gmail.com>
2017-07-26 10:09:34 +0530

    Second commit

--- infix.c
+++ infix.c
@@ -1,13 +1,12 @@
-#include<stdio.h>
-#include<ctype.h>
+ 	#include<stdio.h>
+#include<ctype.h> //for isaplha() function
 
 typedef struct conversion
 {
-	
-	char s[30];
+	char a[30];
 	int top;
-}
-stack;
+}stack;
+
 void push(stack*,char);
 
 char pop(stack*);
@@ -19,7 +18,7 @@ int priority(char);
 int main()
 {
 	char infix[30],postfix[30];
-	printf("\ENTER INFIX EXPRESSION\n");
+	printf("\nENTER INFIX EXPRESSION\n");
 	scanf("%s",infix);
 	convert(infix,postfix);
 	printf("\n\nPOSTFIX EXPRESSION\n%s",postfix);
@@ -35,24 +34,24 @@ void convert(char in[],char post[])
 	for(i=0;in[i]!='\0';i++)
 	{
 		if(isalpha(in[i]))
-		post[j++]=in[i];
+			post[j++]=in[i];
 		if(in[i]=='(')
-		push(&s1,in[i]);
+			push(&s1,in[i]);
 		if(in[i]=='+'||in[i]=='-'||in[i]=='/'||in[i]=='*')
 		{
-			if(s1,top!=-1)
+			if(s1.top!=-1)
 			{
 				opr=pop(&s1);
 				while(priority(opr)>=priority(in[i]))
 				{
-					post[j++]=opr;
-					opr=pop(&s1);
-				}	
+					 post[j++]=opr;
+					 opr=pop(&s1);
+				}
 				push(&s1,opr);
 				push(&s1,in[i]);
 			}
 			else
-			push(&s1,in[i]);
+				push(&s1,in[i]);
 		}
 		if(in[i]==')')
 		{
@@ -65,19 +64,38 @@ void convert(char in[],char post[])
 		}
 	}
 	while(s1.top!=-1)
-	post[j++]=pop(&s1);
+		post[j++]=pop(&s1);
 	post[j]='\0';
-}
-int priority(char in)
+ }
+
+int priority(char c)
 {
-if(in=='$')
- return 3;
-if(in=='+'||in=='-')
- return 2; return 1;
-else
- return 0;
+	 if(c=='$')
+		return 3;
+	 if(c=='/'||c=='*')
+		return 2;
+	 if(c=='+'||c=='-')
+		return 1;
+	 else
+		return 0;
 }
+
+void push(stack *s,char opr)
+{
+	s->top++;
+	s->a[s->top]=opr;
 }
-				
-				
-	
+
+char pop(stack *s)
+{
+	if (s->top==-1)
+	{
+		printf("stack is empty\n");
+		return 0;
+    }
+    else
+    {
+		char data=s->a[s->top--];
+		return data;
+    }
+}
